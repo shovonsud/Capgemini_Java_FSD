d6513f224ee2d6805f414e7e8a80a3673cc10712
Sudipto Dasgupta <shovon.das.gupta@gmail.com>
2022-03-14 02:18:07 +0530

    Java Strings and its functions

---
+++ src/com/cg/day3/StringTrim.java
@@ -0,0 +1,13 @@
+package com.cg.day3;
+
+public class StringTrim {
+
+	public static void main(String[] args) {
+		String str1 = " Learn Java Programming ";
+		String str2 = "Learn\nJava Programming\n\n ";
+		System.out.println(str1.trim());
+		System.out.println(str2.trim());
+
+	}
+
+}
